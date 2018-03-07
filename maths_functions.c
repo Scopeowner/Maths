acfa2786fdde58bb513cc9d8686fe51a01bda3d4
Scopeowner <scopeowner@yahoo.co.uk>
2018-03-07 17:13:58 +0000

    added another conflict

--- maths_functions.c
+++ maths_functions.c
@@ -1,6 +1,6 @@
 //This is the line causing issue in the conflict branch
 
-//This is the second line causing issue in the <*master/conflict> branch
+//This is the second line causing issue in the <conflict> branch
 
 #include <stdio.h>
 
